3803eae6078517d6d6705466fe9997be0da9bd56
Zach Gray <zgray@adobe.com>
2026-01-23 17:58:36 -0800

    initial work on bazel macros with compile time validation and examples

--- tests/ui.rs
+++ tests/ui.rs
@@ -6,5 +6,17 @@
 #[test]
 fn ui_tests() {
     let t = trybuild::TestCases::new();
-    t.compile_fail("tests/ui/*.rs");
+
+    t.compile_fail("tests/ui/cmd_not_in_allowlist.rs");
+    t.compile_fail("tests/ui/missing_absolute_path.rs");
+    t.compile_fail("tests/ui/missing_relative_path.rs");
+    t.compile_fail("tests/ui/raw_string_rejected.rs");
+    t.compile_fail("tests/ui/undefined_env_var.rs");
+    t.compile_fail("tests/ui/undefined_variable.rs");
+
+    #[cfg(feature = "bazel")]
+    {
+        t.compile_fail("tests/ui/bazel_empty_command.rs");
+        t.compile_fail("tests/ui/bazel_invalid_verb.rs");
+    }
 }
